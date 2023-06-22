7f7e0444cc9bdeb2ad941964151d7174d60828a3
fathima-zuhara <118648764+fathima-zuhara@users.noreply.github.com>
2023-06-22 22:24:37 +0400

    Update Algebra_Page.java

--- src/main/java/Pages_WM/Algebra_Page.java
+++ src/main/java/Pages_WM/Algebra_Page.java
@@ -6,7 +6,7 @@ import BasePage_Webmath.BasePage_WM;
 
 public class Algebra_Page extends BasePage_WM{
 
-	public void Algebra_button() {
+	public void algebraButton() {
 		driver.findElement(By.id("navBox-4")).click();
 	}
 }
